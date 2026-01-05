2efdd79fd7439127f1a8053daef9e7524eb9c4a9
Fer Catalano <fernando@hazak.tech>
2026-01-05 16:09:13 -0300

    Add - pusher

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,19 +3,9 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
-const host = import.meta.env.VITE_REVERB_HOST === 'localhost' && window.location.hostname !== 'localhost'
-    ? window.location.hostname
-    : import.meta.env.VITE_REVERB_HOST;
-
-const scheme = window.location.protocol === 'https:' ? 'https' : (import.meta.env.VITE_REVERB_SCHEME ?? 'https');
-const port = import.meta.env.VITE_REVERB_PORT ?? (scheme === 'https' ? 443 : 80);
-
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: host,
-    wsPort: port,
-    wssPort: port,
-    forceTLS: scheme === 'https',
-    enabledTransports: ['ws', 'wss'],
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true
 });
