f9c72502bebb133a8f3126576a6e9e42898b0484
Fer Catalano <fernando@hazak.tech>
2026-01-05 12:30:02 -0300

    Add - fix websocket

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,12 +3,19 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
+const host = import.meta.env.VITE_REVERB_HOST === 'localhost' && window.location.hostname !== 'localhost'
+    ? window.location.hostname
+    : import.meta.env.VITE_REVERB_HOST;
+
+const scheme = window.location.protocol === 'https:' ? 'https' : (import.meta.env.VITE_REVERB_SCHEME ?? 'https');
+const port = import.meta.env.VITE_REVERB_PORT ?? (scheme === 'https' ? 443 : 80);
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wsHost: host,
+    wsPort: port,
+    wssPort: port,
+    forceTLS: scheme === 'https',
     enabledTransports: ['ws', 'wss'],
 });
