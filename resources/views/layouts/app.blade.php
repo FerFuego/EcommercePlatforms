5dd9d00901f81a385936c6f5812edcd97af996d2
Fer Catalano <fernando@hazak.tech>
2026-01-09 17:42:20 -0300

    Add - fav link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -240,7 +240,7 @@
                         </a>
                         <a href="{{ route('favorites.index') }}"
                             class="text-gray-700 hover:text-red-600 font-medium transition-colors">
-                            Favoritos
+                            ⭐ Favoritos
                         </a>
                         <a href="{{ route('cart.index') }}"
                             class="block px-4 py-3 text-gray-700 hover:bg-orange-50 rounded-xl font-medium transition-colors flex items-center justify-between">
