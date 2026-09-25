f296a9ebaf56f3182f5f4a9b8cb896d3a3b98e30
Fer Catalano <fernando@hazak.tech>
2026-09-25 16:16:37 -0300

    fix: comment out Referrer-Policy and enable geolocation in Permissions-Policy

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -14,10 +14,9 @@ class SecurityHeaders
 
         $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
-        $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
+        //$response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
         // Recomendado pero en navegador no funciona bien la geolocalizacion
-        // $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=(self)');
-        $response->headers->set('Permissions-Policy', 'camera=(), microphone=()');
+        $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=(self)');
 
         // HSTS solo debe enviarse bajo HTTPS (RFC 6797) para evitar problemas en desarrollo local
         if ($request->isSecure()) {
