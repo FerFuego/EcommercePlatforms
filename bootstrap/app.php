c1ad0c767ab8a4989f6ec137de14c6f8edfa98f3
Fer Catalano <fernando@hazak.tech>
2026-09-25 16:24:44 -0300

    refactor: move security headers from middleware to Apache .htaccess

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,9 +28,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'api/mercadopago/webhook',
             'api/whatsapp/webhook',
         ]);
-
-        // Security headers
-        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
